c71edb64db9733a633db503b49a46115a6bc76f7
BryaanF <briliantfikri@gmail.com>
2024-12-31 10:17:08 +0700

    first commit

---
+++ frontend/src/pages/testmaterialui.js
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+
+function testmateralui() {
+	return (
+		<div>
+			<Button variant="contained" color="primary">
+				Hello World
+			</Button>
+		</div>
+	);
+} // end function testmateralui
+
+export default testmateralui;
